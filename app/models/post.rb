6316bcb81fe0a343b1eb62c121920b5185839dab
tadeuasarro <tadeutas@gmail.com>
2020-09-24 19:02:39 -0300

    Finish some details all over the project

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
